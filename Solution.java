2af03f22bdfeb0b842898e9485b489588fb126f6
Bống Bống <pieces.nyan@gmail.com>
2024-10-07 16:26:27 +0700

    upload

--- Solution.java
+++ Solution.java
@@ -1,7 +1,10 @@
 import static java.lang.Math.max;
 
 public class Solution {
-    public static class pig {
+
+    private static pig tuan;
+
+    public static class pig extends Animal {
         public static boolean action(String s){
             if(s.equals("walk")) {
                 return true;
@@ -9,7 +12,7 @@ public class Solution {
             return false;
         }
     }
-    public static class duck {
+    public static class duck extends Animal {
         public static boolean action(String s){
             if(s.equals("walk") || s.equals("swin")) {
                 return true;
@@ -17,7 +20,7 @@ public class Solution {
             return false;
         }
     }
-    public static class fish {
+    public static class fish extends Animal {
         public static boolean action(String s){
             if(s.equals("swin")) {
                 return true;
@@ -28,18 +31,21 @@ public class Solution {
     public static void main(String[] args) {
 
         duck dnmd = new duck();
+        dnmd.setName("dnmd");
         fish mao = new fish();
+        mao.setName("mao");
         pig tuan = new pig();
+        tuan.setName("tuan");
         String[] action = {"walk", "swin"};
         for(int i = 0; i < action.length; i++) {
             if(dnmd.action(action[i])) {
-                System.out.println("dnmd" + " can " + action[i]);
+                System.out.println(dnmd.getName() + " can " + action[i]);
             }
             if(mao.action(action[i])) {
-                System.out.println("mao" + " can " + action[i]);
+                System.out.println(mao.getName() + " can " + action[i]);
             }
             if(tuan.action(action[i])) {
-                System.out.println("tuan" + " can " + action[i]);
+                System.out.println(tuan.getName() + " can " + action[i]);
             }
         }
 
